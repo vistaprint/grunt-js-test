f46a2702ab8d6e37e22f4de7ce8ca2447c0ab414
Benjamin Hutchins <ben@hutchins.co>
2013-10-30 16:08:48 -0400

    Added support for "Run all tests" via web service. Cleaned up code a lot. Added back in bootstrap.min.js to support mobile devices. Currently the all tests will break when a custom *.tests.html file is found.

--- server.js
+++ server.js
@@ -7,9 +7,7 @@ var express = require('express'),
 
 var app = express();
 
-var rootFolder = 'C:/vp/playpens/development/subversion/Web/www';
-
-app.configure(function(){
+app.configure(function () {
 	app.set('views', __dirname + '/site');
 	app.locals.pretty = true;
 	app.set('view engine', 'jade');
@@ -20,7 +18,7 @@ app.configure(function(){
 
 	// proxy static javascript files from vp's js-lib
 	app.use('/vp/JS-Lib/js-test-env', express.static(__dirname + '/site/deps'));
-	app.use('/vp/JS-Lib', express.static(rootFolder + '/vp/JS-Lib'));
+	app.use('/vp/JS-Lib', express.static('C:/vp/playpens/development/subversion/Web/www/vp/JS-Lib'));
 });
 
 app.get('/', function (req, res) {
@@ -29,14 +27,27 @@ app.get('/', function (req, res) {
 	});
 });
 
+app.get('/all', function (req, res) {
+	// get an array of all test files to find their dependencies for
+	var testFiles = testUtils.findTests().map(function (test) {
+		return test.file;
+	});
+
+	res.render('all-tests', {
+		deps: findDeps(testFiles),
+	});
+});
+
 app.get('/alive', function (req, res) {
 	res.send('hello world');
 });
 
 app.get('/test', function (req, res) {
+	var deps = findDeps(req.query.js);
+
 	res.render('test', {
 		testFile: req.query.js,
-		deps: findDeps(rootFolder, req.query.js),
+		deps: deps,
 	});
 });
 
