fbc73a7af06934e3daff05c7941fc3340540fc87
Benjamin Hutchins <ben@hutchins.co>
2013-12-04 14:42:16 -0500

    Allow support for mocha-phantom via CLI.

--- site/deps/shared.js
+++ site/deps/shared.js
@@ -2,10 +2,14 @@ mocha.setup('bdd');
 
 window.addEventListener('load', function () {
 	window.assert = chai.assert;
-    window.expect = chai.expect;
+	window.expect = chai.expect;
 
-    // if this is not a require.js test, then run mocha on page load
+	// if this is not a require.js test, then run mocha on page load
 	if (!document.body.dataset.modules) {
-		mocha.run();
+		if (window.mochaPhantomJS) {
+			mochaPhantomJS.run();
+		} else {
+			mocha.run();
+		}
 	}
 }, false);
