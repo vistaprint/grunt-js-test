806b936018e8868fe4c98ec1613b8217d6186579
Benjamin Hutchins <ben@hutchins.co>
2014-08-07 15:50:29 -0400

    Linter enforcement, switched to eslint rather than jshint.

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    eslint: {
+      options: {
+        config: "eslint.json",
+        format: "compact"
+      },
+      "default": {
+        src: ["task/**/*.js"]
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-eslint');
+
+  // Whenever the "test" task is run, run the "nodeunit" task.
+  // grunt.registerTask('test', ['eslint', 'nodeunit']);
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['eslint']);
+
+};
