d9646f7509e507e7289c2eb9d76541a69311089f
Benjamin Hutchins <ben@hutchins.co>
2014-08-25 15:08:14 -0400

    To allow jQuery Ajax requests, we need to end the request when dealing with an OPTIONS request.

--- tasks/lib/server.js
+++ tasks/lib/server.js
@@ -43,6 +43,12 @@ module.exports = function (grunt, options) {
   statics.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+
+    if (req.method === 'OPTIONS') {
+      res.end();
+      return;
+    }
+
     next();
   });
 
