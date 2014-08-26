eb837899744fd5aced4787207459d8dbfb437da6
Benjamin Hutchins <ben@hutchins.co>
2014-08-26 16:12:50 -0400

    Require.js project example using grunt-js-test. Using the same trig.js and unit tests from references example.

---
+++ examples/requirejs/Gruntfile.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    'js-test': {
+      options: {
+        pattern: 'test/*.unittests.js',
+        deps: [
+          // 'http://requirejs.org/docs/release/2.1.14/minified/require.js',
+          'http://requirejs.org/docs/release/2.1.14/comments/require.js',
+          'js/require.config.js',
+          'test/test-bootstrapper.js'
+        ],
+        requirejs: true,
+        moduleRelativeTo: process.cwd()
+      }
+    }
+  });
+
+  // Actually load grunt-js-test
+  // grunt.loadNpmTasks('grunt-js-test');
+  grunt.loadTasks('../../tasks');
+
+  // By default, run all your tests
+  grunt.registerTask('default', ['js-test']);
+
+};
