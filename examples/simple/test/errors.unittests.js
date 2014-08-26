6d179b7295b2c295cfae88261a04189e20f9e36d
Benjamin Hutchins <ben@hutchins.co>
2014-08-26 15:30:58 -0400

    Adding more useless unit tests to the examples/simple for more examples.

---
+++ examples/simple/test/errors.unittests.js
@@ -0,0 +1,8 @@
+describe('Cause errors', function () {
+  it('should be marked pending', function (done) {
+  });
+
+  it('should produce an assert failure', function () {
+    assert.equal(true, false);
+  });
+});
