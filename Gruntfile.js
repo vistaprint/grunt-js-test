5d96f2899f6036a73af8ee697c360be58d97bf33
Laban Eilers <laban@eilers.com>
2018-02-10 11:43:34 -0500

    vscode config, testing deps

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,16 +17,30 @@ module.exports = function (grunt) {
           'Gruntfile.js'
         ]
       }
+    },
+    'js-test': {
+      options: {
+          pattern: 'tests/*.unittests.js',
+          mocha: { 
+              timeout: 6000
+          }
+      }
     }
   });
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-eslint');
 
+  grunt.loadTasks('./tasks');
+
+  grunt.registerTask('foo', function() {
+    grunt.log.write('happened!');
+  });
+
   // Whenever the "test" task is run, run the "nodeunit" task.
   // grunt.registerTask('test', ['eslint', 'nodeunit']);
 
   // By default, lint and run all tests.
-  grunt.registerTask('default', ['eslint']);
+  grunt.registerTask('default', ['eslint', 'js-test']);
 
 };
